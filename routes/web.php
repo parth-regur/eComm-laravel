6f4cd90661e1b1736a207bd728f0aaa1c60fbefe
parth_regur <parth.makvana@regur.net>
2023-05-10 16:14:50 +0530

    Added login with session functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,6 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::post('/login',[UserController::class,'login']);
+Route::get('/',[ProductController::class,'index']);
