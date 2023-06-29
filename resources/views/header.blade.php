07387cc5f56e60716b7994b50f6c03bce925e033
parth-regur <parth.makvana@regur.net>
2023-06-29 16:14:32 +0530

    Added user registration functionality

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -54,7 +54,9 @@
 			<li class="nav-item">
 				<a class="nav-link" href="login/">Login</a>
 			</li>
-
+			<li class="nav-item">
+				<a class="nav-link" href="register/">Register</a>
+			</li>
 			@endif
 
 		</ul>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,28 @@
+@extends('master')
+@section('content')
+
+<div class="row mt-100">
+    <div class="col-sm-4 mx-auto">
+
+        <form action="register" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="exampleInputName">Username</label>
+                <input type="text" name="name" class="form-control" id="exampleInputName">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </form>
+
+    </div>
+</div>
+    
+@endsection
